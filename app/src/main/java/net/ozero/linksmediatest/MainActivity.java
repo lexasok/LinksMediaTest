c3fc398c71683cc0caff65e367dde19cb24ebacf
lexa <sokolov@ozero.net>
2018-04-17 19:28:48 +0300

    RecyclerView created

--- app/src/main/java/net/ozero/linksmediatest/MainActivity.java
+++ app/src/main/java/net/ozero/linksmediatest/MainActivity.java
@@ -2,13 +2,23 @@ package net.ozero.linksmediatest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView = findViewById(R.id.recyclerView);
+        linearLayoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
 
     }
 }
