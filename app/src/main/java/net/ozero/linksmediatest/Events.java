8221fbf9b11e5d0f18a9b879d8f5fa072343ba49
lexa <sokolov@ozero.net>
2018-04-18 18:16:11 +0300

    New data structure added

---
+++ app/src/main/java/net/ozero/linksmediatest/Events.java
@@ -0,0 +1,8 @@
+package net.ozero.linksmediatest;
+
+import java.util.ArrayList;
+
+public class Events {
+
+    ArrayList<Event> events;
+}
--- app/src/main/java/net/ozero/linksmediatest/api/Api.java
+++ app/src/main/java/net/ozero/linksmediatest/api/Api.java
@@ -1,4 +1,15 @@
 package net.ozero.linksmediatest.api;
 
+import net.ozero.linksmediatest.Event;
+
+import java.util.ArrayList;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+
 public interface Api {
+
+    @GET("list.php?category={category}")
+    Call<ArrayList<Event>> events(@Path("category") String category);
 }
