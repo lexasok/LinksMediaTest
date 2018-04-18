39eb70fe1493b6c60c10ed6e75099a4f829f7581
lexa <sokolov@ozero.net>
2018-04-18 18:46:01 +0300

    Field added, gson initialized

--- app/src/main/java/net/ozero/linksmediatest/App.java
+++ app/src/main/java/net/ozero/linksmediatest/App.java
@@ -2,6 +2,31 @@ package net.ozero.linksmediatest;
 
 import android.app.Application;
 
+import com.google.gson.FieldNamingPolicy;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import net.ozero.linksmediatest.api.Api;
+
 public class App extends Application {
 
+    private Api mApi;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Gson gson = new GsonBuilder()
+                .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_DASHES)
+                .setDateFormat("yyyy-MM-dd HH:mm:ss")
+                .create();
+
+
+
+
+    }
+
+    public Api api() {
+        return mApi;
+    }
 }
