3a0c1da3b1aa7669499d853b3d2a9ed6e8598a16
lexa <sokolov@ozero.net>
2018-04-19 02:14:06 +0300

    Design changed

--- app/src/main/java/net/ozero/linksmediatest/MainActivity.java
+++ app/src/main/java/net/ozero/linksmediatest/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+//        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
 
         tabLayout = findViewById(R.id.tabs);
         viewPager = findViewById(R.id.viewPager);
